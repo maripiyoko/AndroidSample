a0d65c9670c735d21c38e55e36482324a9eff27d
Mariko <maripiyok@gmail.com>
2018-11-15 21:11:37 +0900

    migrate to androidx

--- app/src/main/java/net/zuccha/a100days/a100daysofcode/MainActivity.kt
+++ app/src/main/java/net/zuccha/a100days/a100daysofcode/MainActivity.kt
@@ -3,7 +3,7 @@ package net.zuccha.a100days.a100daysofcode
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
+import androidx.core.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
